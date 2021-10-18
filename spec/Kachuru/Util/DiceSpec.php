891944c8cfb902402c48c94690d06ddafc898113
Rupert Jones <ru@kachuru.uk>
2021-10-18 16:48:15 +0100

    Functionality for parsing and performing a dice roll

---
+++ spec/Kachuru/Util/DiceSpec.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace spec\Kachuru\Util;
+
+use Kachuru\Util\Dice;
+use PhpSpec\ObjectBehavior;
+
+class DiceSpec extends ObjectBehavior
+{
+    private const ROLL_ATTEMPTS = 20;
+
+    function it_rolls_six_sides_by_default()
+    {
+        for ($i = 0; $i < self::ROLL_ATTEMPTS; $i++) {
+            $this->roll()->shouldBeAnyOf(1, 2, 3, 4, 5, 6);
+        }
+    }
+
+    function it_rolls_a_d4()
+    {
+        for ($i = 0; $i < self::ROLL_ATTEMPTS; $i++) {
+            $this->roll('d4')->shouldBeAnyOf(1, 2, 3, 4);
+        }
+    }
+
+    function it_rolls_2d6()
+    {
+        for ($i = 0; $i < self::ROLL_ATTEMPTS; $i++) {
+            $this->roll('2d6')->shouldBeAnyOf(2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
+        }
+    }
+
+    function it_roles_3d5()
+    {
+        for ($i = 0; $i < self::ROLL_ATTEMPTS; $i++) {
+            $this->roll('3d5')->shouldBeAnyOf(3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
+        }
+    }
+
+    function it_applies_a_modifier_d4minus1()
+    {
+        for ($i = 0; $i < self::ROLL_ATTEMPTS; $i++) {
+            $this->roll('2d4-1')->shouldBeAnyOf(0, 1, 2, 3, 4, 5, 6, 7);
+        }
+    }
+}
---
+++ src/Kachuru/Util/Dice.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kachuru\Util;
+
+use Kachuru\Util\Exception\InvalidDiceDefinitionException;
+
+class Dice
+{
+    public function roll($definition = 'd6'): int
+    {
+        if (!preg_match('/^(?P<times>[0-9]*)d(?P<sides>[0-9]+)(?P<modifier>\+|\-[0-9]*)?$/i', $definition, $result)) {
+            throw new InvalidDiceDefinitionException(sprintf('The definition "%s" could not be parsed', $definition));
+        }
+
+        $times = (int) $result['times'];
+        if (empty($times)) {
+            $times = 1;
+        }
+
+        return $this->doRoll((int) $result['sides'], $times)
+            + ((array_key_exists('modifier', $result)) ? (int) $result['modifier'] : 0);
+    }
+
+    private function doRoll(int $sides, int $times = 1): int
+    {
+        return array_reduce(range(1, $times), function ($carry) use ($sides): int {
+            return $carry + mt_rand(1, $sides);
+        });
+    }
+}
