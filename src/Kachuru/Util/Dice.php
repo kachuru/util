070374cda7eda2605604eff404500f775f31109a
Rupert Jones <git@kachuru.uk>
2024-01-09 11:55:14 +0000

    Fix issue with Dice without d not working on >2 digits

--- src/Kachuru/Util/Dice.php
+++ src/Kachuru/Util/Dice.php
@@ -8,18 +8,21 @@ use Kachuru\Util\Exception\InvalidDiceDefinitionException;
 
 class Dice
 {
-    private const DICE_DEFINITION_PATTERN = '^(?P<times>[0-9]*)d?(?P<sides>[0-9]+)(?P<modifier>(\+|\-)[0-9]*)?$';
+    private const DICE_DEFINITION_PATTERN = '^(?P<times>[0-9]+)?d?(?P<sides>[0-9]+)(?P<modifier>(\+|\-)[0-9]*)?$';
 
     public function roll(string $definition = 'd6'): int
     {
+        if (is_numeric($definition)) {
+            $definition = 'd' . $definition;
+        }
+
         if (!preg_match(sprintf('/%s/i', self::DICE_DEFINITION_PATTERN), $definition, $result)) {
             throw new InvalidDiceDefinitionException(sprintf('The definition "%s" could not be parsed', $definition));
         }
 
-        $times = (int) $result['times'];
-        if (empty($times)) {
-            $times = 1;
-        }
+        $times = !empty($result['times'])
+            ? intval($result['times'])
+            : 1;
 
         return $this->doRoll((int) $result['sides'], $times)
             + ((array_key_exists('modifier', $result)) ? (int) $result['modifier'] : 0);
